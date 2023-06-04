aadd944c00b59368bbf1955cb0fee93283b53e30
James-Frown <21122@ctca.co.za>
2023-06-04 17:10:30 +0200

    Newest Changes | tried to add an env util but it wasnt working

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,12 +1,25 @@
+import "dotenv/config";
+
+import mongoose from "mongoose";
+
 import express from "express";
 
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello, World");
 });
 
-app.listen(port, () => {
-    console.log("Server running on port " + port);
-});
+const port = process.env.PORT;
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+    .then(() => {
+        console.log("Mongoose: Connected");
+        app.listen(port, () => {
+            console.log("Server: Running");
+            console.log("Port: " + port);
+        });
+    })
+    .catch(console.error);
+
