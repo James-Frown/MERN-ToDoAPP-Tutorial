9321cc457d1a502a76c97c7d03e83c8b2942dae6
James-Frown <21122@ctca.co.za>
2023-06-04 17:38:11 +0200

    Newest Changes |

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World");
+app.get("/", async (req, res) => {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
 });
 
 export default app;
