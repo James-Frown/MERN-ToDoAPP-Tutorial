e880cd962212c9b6833035c3ee2dbf72e65b3d0c
James-Frown <21122@ctca.co.za>
2023-06-05 17:20:49 +0200

    broke the project, need to try later and fix it

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -5,8 +5,15 @@ import NoteModel from "./models/note";
 const app = express();
 
 app.get("/", async (req, res) => {
-    const notes = await NoteModel.find().exec();
-    res.status(200).json(notes);
+    try {
+        const notes = await NoteModel.find().exec();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.log(error);
+        let errorMessage = "An Unknown error occured";
+        if (error instanceof Error) errorMessage = error.message;
+        res.status(500).json({ error: errorMessage });
+    }
 });
 
 export default app;
